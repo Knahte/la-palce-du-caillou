47e6424ab1899def731440cba70616d5045bc479
The one and only waffle toaster <ethan.vermot-desroches@orange.fr>
2024-11-22 22:18:23 +0100

    going back to html
    lol static website ;P

--- Template/footer.php
+++
@@ -1,3 +0,0 @@
-<footer>
-    Ce site a été réalisé par les élèves de la filière <a href="https://www.youtube.com/watch?v=2qBlE2-WL60" target="_blank">MP2I</a> des Classes Préparatoires aux Grandes Ecoles de la cité scolaire Kleber de Strasbourg.
-</footer>
