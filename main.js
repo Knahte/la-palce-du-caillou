74b80d41d9df325981b9e5dbc3e5fe1df24271e9
The one and only waffle toaster <ethan.vermot-desroches@orange.fr>
2024-12-08 20:11:28 +0100

    Fixing redirect URL

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-if (window.location.pathname.endsWith('.html')) {
-    var newUrl = window.location.pathname.replace('.html', '');
-    window.location.replace(newUrl);
-}
-if (window.location.pathname.endsWith('index')) {
-    var newUrl = window.location.pathname.replace('index', '');
-    window.location.replace(newUrl);
-}
+console.log("Hello :P");
